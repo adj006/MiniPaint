bdf1b78db9c6c479d6388df8d364f474f99a7072
Adrian <ajim1989@gmail.com>
2020-04-04 09:31:27 -0700

    Completed setup for when canvas changes size

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -10,4 +10,13 @@ class MyCanvasView(context: Context?) : View(context) {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
