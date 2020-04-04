b466e55933838d19f05a34453a3db1e809e8576d
Adrian <ajim1989@gmail.com>
2020-04-04 09:22:58 -0700

    Added missing colors and styles

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -1,8 +1,13 @@
 package com.example.android.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context?) : View(context) {
-
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
 }
